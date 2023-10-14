b4ddbcda42b21a150384bd44776b7511f44250d2
Kimheang <kimheang135@gmail.com>
2023-10-14 12:09:15 +0700

    chore: add form sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,8 +8,16 @@ exports.getLogin = (req, res, next) => {
   });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false,
+  });
+};
+
 exports.postLogin = (req, res, next) => {
-  User.findById('652413348f8870850e275c7d')
+  User.findById('5bab316ce0a7c75f783cb8a8')
     .then((user) => {
       req.session.isLoggedIn = true;
       req.session.user = user;
@@ -20,6 +28,8 @@ exports.postLogin = (req, res, next) => {
     })
     .catch((err) => console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
