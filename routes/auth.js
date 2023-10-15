d6fb83939a1a4b34e460b324b8baeaf31a205538
Kimheang <kimheang135@gmail.com>
2023-10-15 07:30:54 +0700

    chore: sanitizing data with validation rules

--- routes/auth.js
+++ routes/auth.js
@@ -13,10 +13,14 @@ router.get('/signup', authController.getSignup);
 router.post(
   '/login',
   [
-    body('email').isEmail().withMessage('Please enter a valid email address.'),
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email address.')
+      .normalizeEmail(),
     body('password', 'Password has to be valid.')
       .isLength({ min: 5 })
-      .isAlphanumeric(),
+      .isAlphanumeric()
+      .trim(),
   ],
   authController.postLogin
 );
@@ -39,19 +43,23 @@ router.post(
             );
           }
         });
-      }),
+      })
+      .normalizeEmail(),
     body(
       'password',
       'Please enter a password with only numbers, text and at least 5 characters.'
     )
       .isLength({ min: 5 })
-      .isAlphanumeric(),
-    body('confirmPassword').custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error('Passwords do not match.');
-      }
-      return true;
-    }),
+      .isAlphanumeric()
+      .trim(),
+    body('confirmPassword')
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords do not match.');
+        }
+        return true;
+      }),
   ],
   authController.postSignup
 );
