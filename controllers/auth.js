97cc20485f9246a694dbd7948fcba70322a6fb15
Kimheang <kimheang135@gmail.com>
2023-10-16 12:43:36 +0700

    chore: throw error

--- controllers/auth.js
+++ controllers/auth.js
@@ -93,7 +93,11 @@ exports.postLogin = (req, res, next) => {
           res.redirect('/login');
         });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postSignup = (req, res, next) => {
@@ -121,7 +125,11 @@ exports.postSignup = (req, res, next) => {
     .then((result) => {
       res.redirect('/login');
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postLogout = (req, res, next) => {
