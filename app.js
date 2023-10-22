2d10dac8c3b6638e13b8301d53f340d450830025
Kimheang <kimheang135@gmail.com>
2023-10-22 15:48:55 +0700

    chore: add file compression package

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const flash = require('connect-flash');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 const helmet = require('helmet');
+const compression = require('compression');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -50,6 +51,7 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(helmet());
+app.use(compression());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorage, fileFilter }).single('image'));
